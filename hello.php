9af76175a74e8002202291f401f22315b64b7f95
KaungKhantZayar <kaungkhantzayyar13@gmail.com>
2025-03-17 16:01:07 +0630

    Responsive

--- hello.php
+++ hello.php
@@ -15,7 +15,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
    <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
    <link href='custom.css' rel='stylesheet' type='text/css'>
-<script src="jquery.js" charset="utf-8"></script>
+<script src="app.js" charset="utf-8"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
   </head>
   <style media="screen">
@@ -277,47 +279,126 @@
 .input_box input::-ms-reveal{
   filter: invert(100%);
 }
+@media only screen and (max-width: 600px) {
+  .main_photo{
+    display:none;
+  }
+  .text_col-div{
+    width:100%;
+    padding:20px;
+    margin-top:-60px !important;
+  }
+  .text_size{
+    font-size:30px !important;
+  }
+  .dop{
+    display:none;
+  }
+  .main_icon{
+    margin-left:150px;
+  }
+  .web_text{
+    margin-left:120px;
+    margin-top:10px;
+  }
+  .up{
+    display:none;
+  }
+  .about_photo{
+      width:100%;
+  }
+  .about_text{
+    width:100%;
+  }
+  .main_about{
+    width:100%;
+    margin:0px;
+    padding:20px;
+    margin-top:80px !important;
+  }
+  .about_myphoto{
+    margin-left:90px !important;
+  }
+  .about_text_div{
+    width:100%;
+    margin-left:0px !important;
+    margin-top:-90px;
+  }
+  .about_texts{
+    text-align: center;
+  }
+  .about_story{
+    margin-left:85px;
+  }
+  .about_email{
+    margin-left:85px;
+  }
+  .about_contact{
+    margin-left:200px;
+  }
+  .about_me_text{
+    display: block !important;
+  }
+  .skill_outline{
+      display:none !important;
+  }
+  .main_skill{
+    padding:20px;
+    margin-top:-25px !important;
+  }
+  .contact_div{
+    width:100%;
+    padding:20px;
+  }
+  .contact_google_map_div{
+    width:100% !important;
+    padding:20px;
+  }
+  .google_map{
+    width:100%;
+    height:300px;
+  }
+}
+
   </style>
   <body>
 
 <?php include 'navbar.php'; ?>
-
-    <br>
-
-    <div class="container" style="margin-top:80px;">
-        <div class="d-flex">
-            <div class="col-6">
+<br><br><br><br><br>
+    <div class="container" style="margin-top:-50px;">
+        <div class="d-flex main-div">
+            <div class="text_col-div">
               <div class="" style="margin-top:150px;">
 
                 <div class="d-flex" style="">
                 <h3 style="color:white;">Hello I'm</h3>
-                <div class="ms-3 d-flex" style="font-size:35px; margin-left:10px; color:white; margin-top:-10px; font-family: serif; color:yellow; font-weight:bold; text-shadow:5px 5px 5px lightblue;">
-                  <div id="k" class="me-2 hide">K</div>
-                  <div id="a" class="me-2 hide" style="margin-left:2px;">a</div>
-                  <div id="u" class="me-2 hide" style="margin-left:2px;">u</div>
-                  <div id="n" class="me-2 hide" style="margin-left:2px;">n</div>
-                  <div id="g" class="me-2 hide" style="margin-left:2px;">g</div>
+                <div class="ms-3 d-flex text_size" style="font-size:30px; margin-left:10px; color:white; margin-top:-10px; font-family: serif; color:yellow; font-weight:bold; text-shadow:5px 5px 5px lightblue;">
+                  <div id="k" class="hide">K</div>
+                  <div id="a" class="hide" style="margin-left:0px;">a</div>
+                  <div id="u" class="hide" style="margin-left:0px;">u</div>
+                  <div id="n" class="hide" style="margin-left:0px;">n</div>
+                  <div id="g" class="me-2 hide" style="margin-left:0px;">g</div>
 
-                  <div id="kk" class="me-5 hide" style="margin-left:10px;">K</div>
-                  <div id="h" class="me-2 hide" style="margin-left:2px;">h</div>
-                  <div id="aa" class="me-2 hide" style="margin-left:2px;">a</div>
-                  <div id="nn" class="me-2 hide" style="margin-left:2px;">n</div>
-                  <div id="t" class="me-2 hide" style="margin-left:2px;">t</div>
+                  <div id="kk" class="hide" style="margin-left:0px;">K</div>
+                  <div id="h" class="hide" style="margin-left:0px;">h</div>
+                  <div id="aa" class="hide" style="margin-left:0px;">a</div>
+                  <div id="nn" class="hide" style="margin-left:0px;">n</div>
+                  <div id="t" class="me-2 hide" style="margin-left:0px;">t</div>
 
-                  <div id="z" class="me-2 hide" style="margin-left:10px;">Z</div>
-                  <div id="aaa" class="me-2 hide"style="margin-left:2px;">a</div>
-                  <div id="y" class="me-2 hide" style="margin-left:2px;">y</div>
+                  <div id="z" class="hide" style="margin-left:0px;">Z</div>
+                  <div id="aaa" class="hide"style="margin-left:0px;">a</div>
+                  <div id="y" class="me-2 hide" style="margin-left:0px;">y</div>
 
-                  <div id="yy" class="me-2 hide" style="margin-left:10px;">Y</div>
-                  <div id="aaaa" class="me-2 hide"style="margin-left:2px;">a</div>
-                  <div id="r" class="me-2 hide" style="margin-left:2px;">r</div>
+                  <div id="yy" class="hide" style="margin-left:0px;">Y</div>
+                  <div id="aaaa" class="hide"style="margin-left:0px;">a</div>
+                  <div id="r" class="me- hide" style="margin-left:0px;">r</div>
 
-                  <div id="dop" class="me-2 hide">.</div>
-                  <div id="dopp" class="me-2 hide"style="margin-left:2px;">.</div>
-                  <div id="doppp" class="me-2 hide" style="margin-left:2px;">.</div>
+                  <div id="dop" class="me- hide">.</div>
+                  <div id="dopp" class="me- hide dop"style="margin-left:0px; font-size:30px;">.</div>
+                  <!-- <div id="doppp" class="me- hide dop" style="margin-left:0px; font-size:30px;">.</div> -->
                 </div>
               </div>
-                  <h4 style="color:white;">I am Web Developer</h4>
+                  <h4 style="color:white;" class="web_text">I am Web Developer</h4>
                   <p style="color:gray; text-align:justify; font-size:17px;" class="mt-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore
                   magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo....
                 </p>
@@ -483,56 +564,58 @@
 
                   </script>
 
-                  <div class="">
-                    <a href="icon-link">
+                  <div class="main_icon">
                     <svg xmlns="http://www.w3.org/2000/svg" style="color:white;" width="25" height="25" fill="currentColor" class="bi bi-facebook ms-2 icon-link1" viewBox="0 0 16 16">
                       <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
                     </svg>
-                    </a>
 
-                    <a href="icon-link" style="margin-left:9px;">
                     <svg xmlns="http://www.w3.org/2000/svg" style="color:white;" width="25" height="25" fill="currentColor" class="bi bi-telegram ms-2 icon-link1" viewBox="0 0 16 16">
                       <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.287 5.906q-1.168.486-4.666 2.01-.567.225-.595.442c-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294q.39.01.868-.32 3.269-2.206 3.374-2.23c.05-.012.12-.026.166.016s.042.12.037.141c-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8 8 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629q.14.092.27.187c.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.4 1.4 0 0 0-.013-.315.34.34 0 0 0-.114-.217.53.53 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09"/>
                     </svg>
-                    </a>
 
-                    <a href="icon-link" style="margin-left:9px;">
                     <svg xmlns="http://www.w3.org/2000/svg" style="color:white;"  width="25" height="25" fill="currentColor" class="bi bi-instagram ms-2 icon-link1" viewBox="0 0 16 16">
                       <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
                     </svg>
-                    </a>
                   </div>
                     <div class="mt-5">
                     <button type="button" class="btn about_me_btn" name="button" style="background-color:yellow; color:black;"><b>More About Me</b></button>
                   </div>
                 </div>
                 </div>
-            <div class="col-6">
+
+            <div class="col-6 main_photo">
                   <div class="" style="margin-top:26px;">
                       <a href="#"><img src="image/myphot5.png" alt="" class="photo"></a>
                   </div>
             </div>
         </div>
     </div>
-<br>
+
 <div id="about">
 
 </div>
-  <div class="container" style="margin-top:100px;">
-    <div class="d-flex">
-      <div class="mt-5" style="margin-left:-90px;">
-        <img src="image/peter.jpg" alt="" class="photo">
+
+  <div class="container main_about" style="margin-top:100px;">
+    <div class="row">
+      <h4 style="color:yellow; font-size:28px; display:none;" class="about_texts about_me_text">Aboud Me</h4>
+
+      <div class="col-6 about_photo">
+        <div class="mt-5 about_photo_div" style="margin-left:-90px;">
+          <img src="image/myphoto3.jpg" alt="" class="photo about_myphoto">
+        </div>
       </div>
-      <div class="" style="margin-top:100px; margin-left:90px;">
-        <h4 style="color:yellow; font-size:28px;">Aboud Me</h4>
-        <p style="color:white; text-align:justify; font-size:17px;" class="mt-4">A story of hardwork and preseveracne</p>
-        <p style="color:gray;">Hi I'm KKZY a web developer and designer speciallzing in creating visually angaging user-friendly website. My expertise blends innovative design with robust functionality to bring your digital ideas to life.</p>
-        <p style="color:white; text-align:justify; font-size:17px;" class="mt-4">Contact</p>
-        <p style="color:gray;">Do you have fancy saying hi to me or you want to get started with your project and you need my help? Feel free to contact me.</p>
-        <p style="color:yellow; text-align:justify; font-size:17px;" class="mt-4">www.kaungkhantzayyar13@gmail.com</p>
-        <div class="mt-1">
-        <button type="button" class="btn about_me_btn" name="button"  style="background-color:yellow; color:black;"><b>HLRE ME</b></button>
-      </div>
+      <div class="col-6 about_text">
+        <div class="about_text_div" style="margin-top:60px; margin-left:90px;">
+          <h4 style="color:yellow; font-size:28px;" class="about_texts">Aboud Me</h4>
+          <p style="color:white; text-align:justify; font-size:17px;" class="mt-4 about_story">A story of hardwork and preseveracne</p>
+          <p style="color:gray;" class="about_texts">Hi I'm KKZY a web developer and designer speciallzing in creating visually angaging user-friendly website. My expertise blends innovative design with robust functionality to bring your digital ideas to life.</p>
+          <p style="color:white; text-align:justify; font-size:17px;" class="mt-4 about_contact">Contact</p>
+          <p style="color:gray;" class="about_texts">Do you have fancy saying hi to me or you want to get started with your project and you need my help? Feel free to contact me.</p>
+          <p style="color:yellow; text-align:justify; font-size:17px;" class="mt-4 about_email">www.kaungkhantzayyar13@gmail.com</p>
+          <div class="mt-1">
+          <button type="button" class="btn about_me_btn" name="button"  style="background-color:yellow; color:black;"><b>HLRE ME</b></button>
+        </div>
+        </div>
       </div>
     </div>
   </div>
@@ -541,10 +624,10 @@
 
   </div>
 
-        <div class="container" style="padding-top:150px;">
+        <div class="container main_skill" style="padding-top:150px;">
             <div class="main-title">
               <h1 class="text-center" style="color:white;">My Skills</h1>
-              <div class="skill-border"></div>
+              <div class="skill-border skill_outline"></div>
             </div>
 
             <div class="row">
@@ -632,11 +715,11 @@
 
           </div>
           <br><br><br><br>
-          <div>
+          <!-- <div>
               <h2 style="color:white;" class="text-center"><b>My Project</b></h2>
-          </div>
+          </div> -->
 
-              <div>
+              <!-- <div>
                 <div id="carouselExampleFade" class="carousel slide carousel-fade container" data-ride="carousel">
                 <div class="carousel-inner">
                   <div class="carousel-item active">
@@ -682,11 +765,8 @@
                   <span class="sr-only">Next</span>
                 </a>
                 </div>
-              </div>
+              </div> -->
 
-              <div>
-
-              </div>
 
               <div  id="contact">
 
@@ -697,7 +777,7 @@
 
                 <div class="container mt-5 main_footer_bg">
                   <div class="row">
-                    <div class="col-6">
+                    <div class="col-6 contact_div">
                       <div class="card main_footer_cd">
                         <div class="card-body">
                           <h3>Contact</h3>
@@ -713,15 +793,15 @@
                         </div>
                       </div>
                     </div>
-                    <div class="col-6">
-                      <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d393.1766974619688!2d96.17193952284455!3d16.920606562378563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sNorth%20Okkalapa%20ayer%2022street!5e1!3m2!1sen!2smm!4v1741944145666!5m2!1sen!2smm" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <div class="col-6 contact_google_map_div">
+                      <iframe class="google_map" src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d393.1766974619688!2d96.17193952284455!3d16.920606562378563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sNorth%20Okkalapa%20ayer%2022street!5e1!3m2!1sen!2smm!4v1741944145666!5m2!1sen!2smm" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                     </div>
                   </div>
                 </div>
               <!-- contact -->
 
               <div class="" style="position:fixed; top:580px; left:95%;">
-                <a href="#up">
+                <a href="#up" class="up">
                   <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-arrow-up-square-fill up" viewBox="0 0 16 16">
                     <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0"/>
                   </svg>
@@ -730,7 +810,7 @@
 
               <br><br><br>
 
-<?php include 'footer.php'; ?>
+<?php// include 'footer.php'; ?>
 
   </body>
 </html>
--- navbar.php
+++ navbar.php
@@ -4,6 +4,7 @@
  .menu_btn{
   display: block !important;
   margin-right:50px;
+  cursor: pointer;
 }
 .name{
   margin-left:-50px;
@@ -40,11 +41,19 @@
       <a class="nav-link ms-3 nav-color" href="#contact" style="color:white; font-size:18px;">Contact</a>
     </li>
   </ul>
+  <div class="dropdown">
+      <svg style="display:none; color:yellow;" class="menu_btn dropdown-toggle" data-bs-toggle="dropdown" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-menu-down" viewBox="0 0 16 16">
+        <path d="M7.646.146a.5.5 0 0 1 .708 0L10.207 2H14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h3.793zM1 7v3h14V7zm14-1V4a1 1 0 0 0-1-1h-3.793a1 1 0 0 1-.707-.293L8 1.207l-1.5 1.5A1 1 0 0 1 5.793 3H2a1 1 0 0 0-1 1v2zm0 5H1v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM2 4.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
+      </svg>
+    <ul class="dropdown-menu" style="margin-left:-350px; margin-top:20px;">
+        <li><a class="dropdown-item" href="#home" style="color:gray; ">Home</a></li>
+        <li><a class="dropdown-item" href="#about" style="color:gray;">About Us</a></li>
+        <li><a class="dropdown-item" href="#skill" style="color:gray;">Skill</a></li>
+        <li><a class="dropdown-item" href="#services" style="color:gray;">Services</a></li>
+        <li><a class="dropdown-item" href="#project" style="color:gray;">Project</a></li>
+        <li><a class="dropdown-item" href="#contact" style="color:gray;">Contact</a></li>
+    </ul>
+  </div>
 
-  <a href="#" style="display:none;" class="menu_btn">
-    <svg style="color:yellow;" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-menu-down" viewBox="0 0 16 16">
-      <path d="M7.646.146a.5.5 0 0 1 .708 0L10.207 2H14a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h3.793zM1 7v3h14V7zm14-1V4a1 1 0 0 0-1-1h-3.793a1 1 0 0 1-.707-.293L8 1.207l-1.5 1.5A1 1 0 0 1 5.793 3H2a1 1 0 0 0-1 1v2zm0 5H1v2a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM2 4.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0 4a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"/>
-    </svg>
-  </a>
 
 </nav>
